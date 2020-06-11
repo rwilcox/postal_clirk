4e1f24efde1ae3d78b13409419f03c2ebba369e5
Ryan Wilcox <rwilcox@wilcoxd.com>
2020-06-10 23:17:45 -0400

    first CLI integration test is not at logging level

--- tests/integration/tests.sh
+++ tests/integration/tests.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "check that we can manually set a variable"
 echo "================================"
-LOGGING_LEVEL=debug node index.js run -f tests/integration/fixtures/postal_clirk_testing.postman_collection.json "/The test folder/subitem_echo" -e ECHO_SERVER="https://postman-echo.com/"
+node index.js run -f tests/integration/fixtures/postal_clirk_testing.postman_collection.json "/The test folder/subitem_echo" -e ECHO_SERVER="https://postman-echo.com/"
 
 echo "check that the behavior is different with different log levels"
 echo "================================"
