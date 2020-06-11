a79e198a6a7fd0f904ffd5733be48493c1ae771c
Ryan Wilcox <rwilcox@wilcoxd.com>
2020-06-10 23:09:28 -0400

    move to postman-echo.com

--- tests/integration/tests.sh
+++ tests/integration/tests.sh
@@ -2,5 +2,10 @@
 
 set -e
 
-#node index.js run -f tests/integration/fixtures/postal_clirk_testing.postman_collection.json "/The test folder/subitem_echo" -e ECHO_SERVER="https://812dfb8c-678a-43b5-85b5-dc68a169a8f2.mock.pstmn.io/"
-LOGGING_LEVEL=debug node index.js run -f tests/integration/fixtures/postal_clirk_testing.postman_collection.json "/The test folder/subitem_echo" -e ECHO_SERVER="https://812dfb8c-678a-43b5-85b5-dc68a169a8f2.mock.pstmn.io/"
+echo "check that we can manually set a variable"
+echo "================================"
+LOGGING_LEVEL=debug node index.js run -f tests/integration/fixtures/postal_clirk_testing.postman_collection.json "/The test folder/subitem_echo" -e ECHO_SERVER="https://postman-echo.com/"
+
+echo "check that the behavior is different with different log levels"
+echo "================================"
+LOGGING_LEVEL=debug node index.js run -f tests/integration/fixtures/postal_clirk_testing.postman_collection.json "/The test folder/subitem_echo" -e ECHO_SERVER="https://postman-echo.com/"
