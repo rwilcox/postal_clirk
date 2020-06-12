418172a6d3271c523c8e32b402205dce36e4e979
Ryan Wilcox <rwilcox@wilcoxd.com>
2020-06-11 21:48:34 -0400

    test how we respond to a variable being undefined

--- tests/integration/tests.sh
+++ tests/integration/tests.sh
@@ -9,3 +9,7 @@ node index.js run -f tests/integration/fixtures/postal_clirk_testing.postman_col
 echo "check that the behavior is different with different log levels"
 echo "================================"
 LOGGING_LEVEL=debug node index.js run -f tests/integration/fixtures/postal_clirk_testing.postman_collection.json "/The test folder/subitem_echo" -e ECHO_SERVER="https://postman-echo.com/"
+
+echo "check behavior when we forget a variable"
+echo "================================"
+node index.js run -f tests/integration/fixtures/postal_clirk_testing.postman_collection.json "/The test folder/subitem_var" | grep "\"DA_SERVER\" not found"
